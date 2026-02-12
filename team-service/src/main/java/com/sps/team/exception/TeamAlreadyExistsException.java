b025d04ba0b9c6bc8ec6816f09361034f73fb429
Rajitha Basnayake <141009392+RajithaBasnayake@users.noreply.github.com>
2026-02-12 17:02:21 +0530

    Add TeamAlreadyExistsException class
    This exception is thrown when a team with the same name already exists.

--- team-service/src/main/java/com/sps/team/exception/TeamAlreadyExistsException.java
+++ team-service/src/main/java/com/sps/team/exception/TeamAlreadyExistsException.java
@@ -1,1 +1,17 @@
+package com.sps.team.exception;
 
+/**
+ * Team Already Exists Exception
+ *
+ * Thrown when attempting to create a team with a name that already exists
+ */
+public class TeamAlreadyExistsException extends RuntimeException {
+
+    public TeamAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public TeamAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
